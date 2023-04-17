29cf05b31f22ed1e7fcc360f5026bf7d4841f891
vitaliy <vitaliy@heliotrope.digital>
2023-04-17 16:19:01 +0300

    removed unused state

--- src/Convert.tsx
+++ src/Convert.tsx
@@ -1,23 +1,22 @@
 import axios from "axios";
-import { useState, ChangeEvent } from "react";
+import { ChangeEvent } from "react";
 
 export const Convert = () => {
-  const [file, setFile] = useState<File>();
-
   const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFile = e.target.files?.[0];
     if (selectedFile) {
-      setFile(selectedFile);
-      console.log(selectedFile);
-
       // Отправка файла на сервер и получение результата
       const formData = new FormData();
       formData.append("file", selectedFile);
 
       try {
-        const response = await axios.post("http://localhost:3001/convert", formData, {
-          responseType: "blob",
-        });
+        const response = await axios.post(
+          "http://localhost:3001/convert",
+          formData,
+          {
+            responseType: "blob",
+          }
+        );
 
         if (response.status === 200) {
           const blob = response.data;
