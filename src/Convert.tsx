36144fd8e01732c409687ac33fda5352fbc1dc5e
vitaliy <vitaliy@heliotrope.digital>
2023-04-17 16:31:02 +0300

    implement simultaneous multi-file conversion

--- src/Convert.tsx
+++ src/Convert.tsx
@@ -2,39 +2,45 @@ import axios from "axios";
 import { ChangeEvent } from "react";
 
 export const Convert = () => {
+  const convertFile = async (file: File) => {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await axios.post(
+        "http://localhost:3001/convert",
+        formData,
+        {
+          responseType: "blob",
+        }
+      );
+
+      if (response.status === 200) {
+        const blob = response.data;
+        const url = URL.createObjectURL(blob);
+        const link = document.createElement("a");
+        link.href = url;
+        link.download = `${file.name.split(".")[0]}_converted.mp3`;
+        link.click();
+        URL.revokeObjectURL(url);
+      } else {
+        console.error("Error converting the file");
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
     const selectedFiles = e.target.files;
     if (selectedFiles) {
-      // Обрабатываем каждый файл в массиве файлов
-      for (const file of selectedFiles) {
-        // Отправка файла на сервер и получение результата
-        const formData = new FormData();
-        formData.append("file", file);
+      // Создаем массив промисов для каждого файла
+      const convertPromises = Array.from(selectedFiles).map((file) =>
+        convertFile(file)
+      );
 
-        try {
-          const response = await axios.post(
-            "http://localhost:3001/convert",
-            formData,
-            {
-              responseType: "blob",
-            }
-          );
-
-          if (response.status === 200) {
-            const blob = response.data;
-            const url = URL.createObjectURL(blob);
-            const link = document.createElement("a");
-            link.href = url;
-            link.download = `${file.name.split(".")[0]}_converted.mp3`;
-            link.click();
-            URL.revokeObjectURL(url);
-          } else {
-            console.error("Error converting the file");
-          }
-        } catch (err) {
-          console.error(err);
-        }
-      }
+      // Выполняем все промисы параллельно
+      await Promise.all(convertPromises);
     }
   };
 
