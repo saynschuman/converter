9b4f3513cd2218d390f2ae692e77f289706b48cc
vitaliy <vitaliy@heliotrope.digital>
2023-04-17 16:27:18 +0300

    multiple files

--- src/Convert.tsx
+++ src/Convert.tsx
@@ -3,37 +3,40 @@ import { ChangeEvent } from "react";
 
 export const Convert = () => {
   const onChange = async (e: ChangeEvent<HTMLInputElement>) => {
-    const selectedFile = e.target.files?.[0];
-    if (selectedFile) {
-      // Отправка файла на сервер и получение результата
-      const formData = new FormData();
-      formData.append("file", selectedFile);
+    const selectedFiles = e.target.files;
+    if (selectedFiles) {
+      // Обрабатываем каждый файл в массиве файлов
+      for (const file of selectedFiles) {
+        // Отправка файла на сервер и получение результата
+        const formData = new FormData();
+        formData.append("file", file);
 
-      try {
-        const response = await axios.post(
-          "http://localhost:3001/convert",
-          formData,
-          {
-            responseType: "blob",
+        try {
+          const response = await axios.post(
+            "http://localhost:3001/convert",
+            formData,
+            {
+              responseType: "blob",
+            }
+          );
+
+          if (response.status === 200) {
+            const blob = response.data;
+            const url = URL.createObjectURL(blob);
+            const link = document.createElement("a");
+            link.href = url;
+            link.download = `${file.name.split(".")[0]}_converted.mp3`;
+            link.click();
+            URL.revokeObjectURL(url);
+          } else {
+            console.error("Error converting the file");
           }
-        );
-
-        if (response.status === 200) {
-          const blob = response.data;
-          const url = URL.createObjectURL(blob);
-          const link = document.createElement("a");
-          link.href = url;
-          link.download = "converted.mp3";
-          link.click();
-          URL.revokeObjectURL(url);
-        } else {
-          console.error("Error converting the file");
+        } catch (err) {
+          console.error(err);
         }
-      } catch (err) {
-        console.error(err);
       }
     }
   };
 
-  return <input type="file" onChange={onChange} />;
+  return <input type="file" multiple onChange={onChange} />;
 };
