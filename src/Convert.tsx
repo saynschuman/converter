7657f26a15b7c9cefcd9c3b966425acd74272556
vitaliy <vitaliy@heliotrope.digital>
2023-04-17 13:38:48 +0300

    loading ogg file

--- src/Convert.tsx
+++ src/Convert.tsx
@@ -1,1 +1,15 @@
-export const Convert = () => <>hello!</>;
+import { useState, ChangeEvent } from "react";
+
+export const Convert = () => {
+  const [file, setFile] = useState<File>();
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedFile = e.target.files?.[0];
+    if (selectedFile) {
+      setFile(selectedFile);
+      console.log(selectedFile);
+    }
+  };
+
+  return <input type="file" onChange={onChange} />;
+};
